b6b430bd08769b159d46f7f756d27144cae744cb
Liu Cheng <lc504906985@gmail.com>
2016-10-16 16:32:34 +0800

    refactor by eslint

--- server/course.js
+++ server/course.js
@@ -3,31 +3,31 @@ var Courses = collection.Courses;
 var Ids = collection.Ids;
 
 var saveCourse = function(uid, name, info) {
-    var course = {};
-    course.uid = uid;
-    course.name = name;
-    course.info = info;
-    //TODO chapter null
-    if (!Ids.findOne({name:"course"})) {
-        Ids.insert({name:"course", id:1000000});
-    }
-    id = Ids.findOne({name:"course"});
-    course.qrcodeid = id.id + 1;
-    Ids.update({name:"course"}, {$inc:{id: 1}});
-    Courses.insert(course);
-}
+  var course = {};
+  course.uid = uid;
+  course.name = name;
+  course.info = info;
+  //  TODO chapter null
+  if (!Ids.findOne({ name: "course" })) {
+    Ids.insert({ name: "course", id: 1000000 });
+  }
+  id = Ids.findOne({ name: "course" });
+  course.qrcodeid = id.id + 1;
+  Ids.update({ name: "course" }, { $inc: { id: 1 } });
+  Courses.insert(course);
+};
 
 var teacherCourse = function(uid) {
-    return Courses.find({uid:uid}).fetch();
-}
+  return Courses.find({ uid: uid }).fetch();
+};
 
 var studentCourse = function(openid) {
-    return Courses.find({student:openid}).fetch();
-}
+  return Courses.find({ student: openid }).fetch();
+};
 
 var courseInfo = function(id) {
-    return Courses.findOne({_id:id});
-}
+  return Courses.findOne({ _id: id });
+};
 
 exports.saveCourse = saveCourse;
 exports.teacherCourse = teacherCourse;
