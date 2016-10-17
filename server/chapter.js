bf0fb22e9174062551a728a0dbd272d14fe18607
Liu Cheng <lc504906985@gmail.com>
2016-10-17 13:08:40 +0800

    add jsdoc for chapter

--- server/chapter.js
+++ server/chapter.js
@@ -1,19 +1,35 @@
 var collection = require('./collection.js');
 var Chapters = collection.Chapters;
 
+/**
+ * save chapter info to database.
+ * @param  {String} cid Courses._id
+ * @param  {String} name chapter name
+ * @param  {String} info chapter info
+ * @returns {NULL} NULL
+ */
 var saveChapter = function(cid, name, info) {
   var chapter = {};
   chapter.cid = cid;
   chapter.name = name;
   chapter.info = info;
-  // TODO chapter null
   Chapters.insert(chapter);
 };
 
+/**
+ * get a course's chapterList
+ * @param  {String} cid Courses._id
+ * @returns {Array} chapter list
+ */
 var courseChapters = function(cid) {
   return Chapters.find({ cid: cid }).fetch();
 };
 
+/**
+ * get Chapters by _id.
+ * @param  {String} id Chapters._id
+ * @returns {Object} chapter detail
+ */
 var chapterInfo = function(id) {
   return Chapters.findOne({ _id: id });
 };
