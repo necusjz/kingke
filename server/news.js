0cc5814e04a8d375d00880bd2da9659fdc763e7d
Liu Cheng <lc504906985@gmail.com>
2016-10-17 17:59:36 +0800

    js doc

--- server/news.js
+++ server/news.js
@@ -1,6 +1,16 @@
 var collection = require('./collection.js');
 var News = collection.News;
 
+/**
+ * add news.
+ * @param  {String} openid User.openid
+ * @param  {String} infoBegin begin info
+ * @param  {String} course course name
+ * @param  {String} teacher teacher name
+ * @param  {String} time time
+ * @param  {String} infoEnd end info
+ * @returns {NULL} NULL
+ */
 var saveNews = function(openid, infoBegin, course, teacher, time, infoEnd) {
   var news = {};
   news.openid = openid;
@@ -12,6 +22,11 @@ var saveNews = function(openid, infoBegin, course, teacher, time, infoEnd) {
   News.insert(news);
 };
 
+/**
+ * get user's news
+ * @param  {String} openid User.openid
+ * @returns {Array} news list
+ */
 var userNews = function(openid) {
   return News.find({ openid: openid }).fetch();
 };
