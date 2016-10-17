16ed136e19cdc53c9ae2a1fd669b7f565dcfc054
Liu Cheng <lc504906985@gmail.com>
2016-10-17 20:11:03 +0800

    change config.js

--- server/config.js
+++ server/config.js
@@ -1,8 +1,8 @@
 // Warning!!!
 // Do Not Commit this file!!!
-exports.token = "lc504906985";
-exports.appID = "wx691c4f6cf06e6e0c";
-exports.appsecret = "b5291d1e3eb220cb65897b5f5986a742";
-exports.url = "sa516170.mc2lab.com";
-exports.notify_template_id = "WgxvH92neoh7bW-0jXjV9vwAFGmDnilnRbRSq4PDuR8";
-exports.follow_template_id = "zCkOk9BSDwbSwXCS6QJjqClwp-belYoMSE8lb33_UJc";
+exports.token = 'YOUR_TOKEN';
+exports.appID = 'YOUR_APPID';
+exports.appsecret = 'YOUR_APPSECRET';
+exports.url = 'YOUR_DOMAIN_NAME';
+exports.notify_template_id = 'YOUR_TEMPLET_ID';
+exports.follow_template_id = 'YOUR_TEMPLET_ID';
