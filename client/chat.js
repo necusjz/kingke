4ac0d3508c2c50b9a2cffce51b5b3388061d00d5
Liu Cheng <lc504906985@gmail.com>
2016-11-02 09:45:29 +0800

    test chat

---
+++ client/chat.js
@@ -0,0 +1,28 @@
+import './chat.html';
+
+Router.route('/chat', function() {
+  Template.messages.helpers({
+    messages: function() {
+      return Messages.find({}, { sort: { time: -1 } });
+    }
+  });
+
+  Template.input.events = {
+    'keydown input#message': function(event) {
+      if (event.which === 13) { // 13 is the enter key event
+        var name = 'Anonymous';
+        var message = document.getElementById('message');
+        if (message.value !== '') {
+          Messages.insert({
+            name: name,
+            message: message.value,
+            time: Date.now(),
+          });
+
+          document.getElementById('message').value = '';
+          message.value = '';
+        }
+      }
+    }
+  };
+});
