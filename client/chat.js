2790ac88b69e403f1b706aae06265f3c5d68c6ea
Liu Cheng <lc504906985@gmail.com>
2016-11-02 12:43:37 +0800

    fix bug

--- client/chat.js
+++ client/chat.js
@@ -1,28 +1,30 @@
 import './chat.html';
 
+Messages = new Mongo.Collection('messages');
+
 Router.route('/chat', function() {
   Template.messages.helpers({
     messages: function() {
       return Messages.find({}, { sort: { time: -1 } });
     }
   });
+});
 
-  Template.input.events = {
-    'keydown input#message': function(event) {
-      if (event.which === 13) { // 13 is the enter key event
-        var name = 'Anonymous';
-        var message = document.getElementById('message');
-        if (message.value !== '') {
-          Messages.insert({
-            name: name,
-            message: message.value,
-            time: Date.now(),
-          });
+Template.input.events = {
+  'keydown input#message': function(event) {
+    if (event.which === 13) { // 13 is the enter key event
+      var name = 'Anonymous';
+      var message = document.getElementById('message');
+      if (message.value !== '') {
+        Messages.insert({
+          name: name,
+          message: message.value,
+          time: Date.now(),
+        });
 
-          document.getElementById('message').value = '';
-          message.value = '';
-        }
+        document.getElementById('message').value = '';
+        message.value = '';
       }
     }
-  };
-});
+  }
+};
